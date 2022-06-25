0b188c60f30ef9066b3caaa964c50406740fff65
RuthMaria <ruth-15rms@hotmail.com>
2022-06-25 18:03:12 -0300

    feat: finish video and add routes

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ptBR from "date-fns/locale/pt-BR";
 import { format, isPast } from "date-fns";
 import { CheckCircle, Lock } from "phosphor-react";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -19,11 +20,12 @@ const Lesson = (props: LessonProps) => {
       locale: ptBR,
     }
   );
+
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-580 p-4 mt-2">
+      <div className="rounded border border-gray-580 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -44,7 +46,7 @@ const Lesson = (props: LessonProps) => {
 
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   );
 };
 
